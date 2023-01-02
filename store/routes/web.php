39ecf4824c12d6a4a1ff6577e295ae368019f962
Dok2314 <koversundanil@gmail.com>
2023-01-02 19:58:42 +0200

    First step

--- store/routes/web.php
+++ store/routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers as Controllers;
 
+Route::get('/', [Controllers\MainController::class, 'index'])->name('index');
+Route::get('/categories', [Controllers\MainController::class, 'categories'])->name('categories');
 
-Route::get('/test', function () {
-    dd('test');
-});
+Route::get('/basket', [Controllers\MainController::class, 'basket'])->name('basket');
+
+Route::get('/{category:slug}', [Controllers\MainController::class, 'category'])->name('category');
+
+Route::get('/mobiles/{product:slug}', [Controllers\MainController::class, 'product'])->name('product');
+
+Route::get('/basket/place', [Controllers\MainController::class, 'basketPlace'])->name('basket-place');
