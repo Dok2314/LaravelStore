30d26bd58846394853250e4b9ead6eb402aabc3b
Dok2314 <koversundanil@gmail.com>
2023-01-18 20:46:54 +0200

    Added new tables, models and relations

---
+++ store/database/migrations/2023_01_18_182102_create_skus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')
+                ->constrained('products')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->unsignedInteger('count')->default(0);
+            $table->double('price')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skus');
+    }
+};
